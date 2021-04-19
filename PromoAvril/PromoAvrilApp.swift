cc6af7d45be341066d2f0adb612eb9c374f787aa
Adrien <adrnsmn@gmail.com>
2021-04-19 13:33:58 +0200

    Initial Commit

---
+++ PromoAvril/PromoAvrilApp.swift
@@ -0,0 +1,17 @@
+//
+//  PromoAvrilApp.swift
+//  PromoAvril
+//
+//  Created by Admin on 19/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct PromoAvrilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
